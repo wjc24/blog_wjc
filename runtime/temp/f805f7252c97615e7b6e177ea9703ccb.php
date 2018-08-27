4884263e2f89ba67dbab342b2f487eed54a86bf8
wangjunchao <378350151@qq.com>
2018-08-27 15:45:16 +0800

    wjc

---
+++ runtime/temp/f805f7252c97615e7b6e177ea9703ccb.php
@@ -0,0 +1,260 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:66:"D:\work\blog_wjc\public/../application/index\view\index\index.html";i:1533782241;s:58:"D:\work\blog_wjc\application\index\view\common\public.html";i:1532053272;}*/ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>主页</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+<link href="/assets/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/assets/css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script src="/assets/js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+
+</head>
+<body>
+
+<!-- banner-body -->
+	<div class="banner-body">
+		<div class="container">
+			<div class="banner-body-content">
+			<div class="col-xs-3 banner-body-left">
+				<div class="logo">
+					<h1><a href="index.html">Hello <span>Cy</span></a></h1>
+				</div>
+				<div class="top-nav">
+					<nav class="navbar navbar-default">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+						  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						  </button>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+							<nav class="stroke">
+								<ul class="nav navbar-nav">
+									<li class="active"><a href="index.html"><i class="home"></i>主页</a></li>
+									<li><a href="/index/photo" class="hvr-underline-from-left"><i class="picture1"></i>照片</a></li>
+									<li><a href="/index/article" class="hvr-underline-from-left"><i class="edit1"></i>文章</a></li>
+									<li><a href="/index/music" class="hvr-underline-from-left"><i class="text-size1"></i>音乐</a></li>
+									<li><a href="/index/video" class="hvr-underline-from-left"><i class="envelope1"></i>视频</a></li>
+								</ul>
+							</nav>
+						</div>
+						<!-- /.navbar-collapse -->
+					</nav>
+				</div>
+			</div>
+			<div class="col-xs-9 banner-body-right">
+				<div class="wmuSlider example1">
+					<div class="wmuSliderWrapper">
+						<article style="position: absolute; width: 100%; opacity: 0;">
+							<div class="banner-wrap">
+								<div class="banner">
+									<img src="/assets/img/1.jpg" alt=" " class="img-responsive" />
+								</div>
+							</div>
+						</article>
+						<article style="position: absolute; width: 100%; opacity: 0;">
+							<div class="banner-wrap">
+								<div class="banner1">
+									<img src="/assets/img/2.jpg" alt=" " class="img-responsive" />
+								</div>
+							</div>
+						</article>
+						<article style="position: absolute; width: 100%; opacity: 0;">
+							<div class="banner-wrap">
+								<div class="banner2">
+									<img src="/assets/img/3.jpg" alt=" " class="img-responsive" />
+								</div>
+							</div>
+						</article>
+					</div>
+				</div>
+					<script src="/assets/js/jquery.wmuSlider.js"></script>
+					  <script>
+						$('.example1').wmuSlider();
+					 </script>
+				<div class="banner-bottom">
+					<div class="col-md-4 banner-left">
+						<div class="col-xs-3 banner-left1">
+							<div class="banner-left11">
+								<span> </span>
+							</div>
+						</div>
+						<div class="col-xs-9 banner-right1">
+							<h3>cupidatat proi</h3>
+						</div>
+						<div class="clearfix"> </div>
+						<p>Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui.</p>
+					</div>
+					<div class="col-md-4 banner-left">
+						<div class="col-xs-3 banner-left1">
+							<div class="banner-left22">
+								<span> </span>
+							</div>
+						</div>
+						<div class="col-xs-9 banner-right1">
+							<h3>sint occaecat</h3>
+						</div>
+						<div class="clearfix"> </div>
+						<p>Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui.</p>
+					</div>
+					<div class="col-md-4 banner-left">
+						<div class="col-xs-3 banner-left1">
+							<div class="banner-left33">
+								<span> </span>
+							</div>
+						</div>
+						<div class="col-xs-9 banner-right1">
+							<h3>cupida quisu</h3>
+						</div>
+						<div class="clearfix"> </div>
+						<p>Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui.</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<div class="copyrights">Collect from <a href="http://www.cssmoban.com/" >网页模板</a></div>
+			<div class="clearfix"> </div>
+			<div class="col-xs-3 banner-body-left">
+				<div class="latest-news">
+					<h2>My  Baal</h2>
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+					  <div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingOne">
+						  <h4 class="panel-title">
+							<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+							  Kobe Bryant
+							</a>
+						  </h4>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+						  <div class="panel-body">
+							  Have you seen Losangeles at four in the morning ?
+						  </div>
+						</div>
+					  </div>
+					  <div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingTwo">
+						  <h4 class="panel-title">
+							<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								Cristiano Ronaldo dos Santos Aveiro
+							</a>
+						  </h4>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+						  <div class="panel-body">
+							  Only God knows who is working hard and who deserves repayment. God won't always fall asleep.
+						  </div>
+						</div>
+					  </div>
+					  <div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingThree">
+						  <h4 class="panel-title">
+							<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+								Taylor Swift
+							</a>
+						  </h4>
+						</div>
+						<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+						  <div class="panel-body">
+							  Come morning light.You and i'll be safe and sound.
+						  </div>
+						</div>
+					  </div>
+					</div>
+
+					<h3>My motto</h3>
+					<p>人类是唯一会交易的动物。因为没有狗会拿骨头去交换。<br>
+						Man is an only animal that makes bargains;no dog exchanges bones with another.</p>
+				</div>
+			</div>
+				
+<div class="col-xs-9 banner-body-right">
+	<div class="msg-text">
+		<div class="col-xs-2 msg-text-left">
+			<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
+		</div>
+		<div class="col-xs-10 msg-text-right">
+			<p>生活就像一盒巧克力，你永远不知道下一块会是什么味道。<br>
+				Life was a box of chocolates,you never know what you're gonna get.</p>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+	<div class="msg-text-bottom">
+		<div class="col-md-4 msg-text-bottom-left">
+			<figure class="effect-winston">
+				<a href="/index/article/detail"><img src="/assets/img/4.jpg" alt=" " class="img-responsive" />
+					<figcaption>
+
+					</figcaption>
+				</a>
+			</figure>
+
+			<h3><a href="/index/article/detail">vel illum qui dolorem</a></h3>
+			<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+				consectetur, adipisci velit, sed quia.</p>
+		</div>
+		<div class="col-md-4 msg-text-bottom-left">
+			<figure class="effect-winston">
+				<a href="/index/article/detail"><img src="/assets/img/5.jpg" alt=" " class="img-responsive" />
+					<figcaption>
+
+					</figcaption>
+				</a>
+			</figure>
+			<h3><a href="/index/article/detail">quia dolor sit amet</a></h3>
+			<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+				consectetur, adipisci velit, sed quia.</p>
+		</div>
+		<div class="col-md-4 msg-text-bottom-left">
+			<figure class="effect-winston">
+				<a href="/index/article/detail"><img src="/assets/img/6.jpg" alt=" " class="img-responsive" />
+					<figcaption>
+
+					</figcaption>
+				</a>
+			</figure>
+			<h3><a href="/index/article/detail">porro quisquam est</a></h3>
+			<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+				consectetur, adipisci velit, sed quia.</p>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+
+			<!--<div class="col-xs-9 banner-body-right">-->
+
+			<!--</div>-->
+
+			<div class="clearfix"> </div>
+			<div class="footer">
+				<div class="footer-left">
+					<ul>
+						<li><a href="#">Privacy Policy</a>|</li>
+						<li><a href="#">Terms of Use</a>|</li>
+						<li><a href="mail.html">Contact Us</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		</div>
+	</div>
+<!-- //banner-body -->
+<!-- for bootstrap working -->
+	<script src="/assets/js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+</body>
+</html>
